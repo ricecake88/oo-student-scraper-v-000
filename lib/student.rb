d797f73d2e413eae69527264bbe850710a98a57d
Grace Shih <vocalise@gmail.com>
2018-05-25 03:07:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     student_indexes = []
-    Scraper.scrape_index_page()
+    s = Scraper.new
   end
 
   def add_student_attributes(attributes_hash)
