afb3eb6e29745e90da9a485b8bde164fc4cb2cb9
Grace Shih <vocalise@gmail.com>
2018-05-25 03:10:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      Student.new(student[:name], student[:location])
+      Student.new(student)
     end
   end
 
