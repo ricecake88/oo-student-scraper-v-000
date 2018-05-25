46b673f44164ee5e41e581545758a71061bc6c90
Grace Shih <vocalise@gmail.com>
2018-05-25 03:21:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,12 +16,13 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    @twitter = attributes_hash[:twitter]
-    @bio = attributes_hash[:bio]
-    @linkedin = attributes_hash[:linkedin]
-    @github = attributes_hash[:github]
-    @blog = attributes_hash[:blog]
-    @profile_quote = attributes_hash[:profile_quote]
+    attributes_hash.each {|key,value| self.send(("#{key}="), value)}
+    #@twitter = attributes_hash[:twitter]
+    #@bio = attributes_hash[:bio]
+    #@linkedin = attributes_hash[:linkedin]
+    #@github = attributes_hash[:github]
+    #@blog = attributes_hash[:blog]
+    #@profile_quote = attributes_hash[:profile_quote]
   end
 
   def self.all
